417a15f8c5ca641f3365c506e9fa6ca4fd920ceb
Nathanael Burt <nathanael.burt@gmail.com>
2014-03-24 15:17:41 -0600

    user can add new items and view them
    Signed-off-by: Mark Evans <twenty4play@gmail.com>

--- appy.rb
+++ appy.rb
@@ -3,7 +3,18 @@ require 'sinatra/base'
 
 class Appy < Sinatra::Base
 
+  FOOD = []
+
   get '/' do
     erb :index
+  end
+
+  get '/new-item' do
+    erb :new_items
+  end
+
+  post '/' do
+    FOOD << params[:new_item]
+    redirect '/'
   end
 end
--- spec/items_spec.rb
+++ spec/items_spec.rb
@@ -7,7 +7,15 @@ Capybara.app = Appy
 feature "Manage items" do
   scenario "user can view and add items" do
     visit "/"
+    expect(page).to have_title("Food Repository")
+
     expect(page).to have_content("Submit New Item")
+
+    expect(page).to_not have_content("Mac n' Cheese")
+
     click_link("Submit New Item")
+    fill_in "new_item", with: "Mac n' Cheese"
+    click_on("Create Item")
+    expect(page).to have_content("Mac n' Cheese")
   end
 end
